d406f4020a6764725c90fdeae7bdb11615cf21a4
shoeb abedin <56837541+shoebabedin12@users.noreply.github.com>
2022-07-03 22:44:01 +0600

    first commit

--- src/components/Home/MainHomePage.js
+++ src/components/Home/MainHomePage.js
@@ -8,10 +8,13 @@ const MainHomePage = (props) => {
         <div className="col-md-2">
           <SideNav active="home" user={props.user}/>
         </div>
-        <div className="col-md-5">
+        <div className="col-md-4">
           <h2>main body</h2>
         </div>
-        <div className="col-md-5">
+        <div className="col-md-3">
+          <h2>main body</h2>
+        </div>
+        <div className="col-md-3">
           <h2>main body</h2>
         </div>
       </div>
